e1d565b5eeaf25debb4cf4c08a63bb4341d761fa
HitoTech <HitoTech@users.noreply.github.com>
2018-03-01 23:58:20 +0100

    Clean Raspbian

--- system_init.sh
+++ system_init.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-apt-get update && apt-get upgrade -y
+# remove unused software
+apt-get update && \
+apt-get purge -y wolfram-engine libreoffice* minecraft-pi sonic-pi && \
+apt-get clean -y && \
+apt-get autoremove -y
+
+# update system
+apt-get upgrade -y && \
+apt-get dist-upgrade -y
 
 apt-get install -y qiv
