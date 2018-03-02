b33511dc8c18d84ac7fea96ad967077a728291b8
HitoTech <HitoTech@users.noreply.github.com>
2018-03-02 18:05:29 +0100

    Clean the system

--- system_init.sh
+++ system_init.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
+set -x
+
 # remove unused software
 apt-get update && \
-apt-get purge -y wolfram-engine libreoffice* minecraft-pi sonic-pi && \
+apt-get purge -q -y wolfram-engine \
+	libreoffice* \
+	minecraft-pi \
+	sonic-pi \
+	oracle-java* openjdk* \
+	python-picraft python3-picraft \
+	scratch* nuscratch \
+	python3-pygame python-pygame \
+	greenfoot && \
 apt-get clean -y && \
-apt-get autoremove -y
+apt-get autoremove -y && \
 
 # update system
 apt-get upgrade -y && \
-apt-get dist-upgrade -y
+apt-get dist-upgrade -y && \
 
 apt-get install -y qiv
